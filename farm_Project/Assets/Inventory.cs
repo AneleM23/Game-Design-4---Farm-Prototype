a5eaca40677c5338a2063811bd00d04266bca973
AneleM23 <116083724+AneleM23@users.noreply.github.com>
2024-04-02 13:33:15 +0200

    A

--- farm_Project/Assets/Inventory.cs
+++ farm_Project/Assets/Inventory.cs
@@ -8,6 +8,19 @@ public class Inventory : MonoBehaviour
 
     public GameObject inventoryUI;
 
+
+    public class Slot
+    {
+        public Sprite icon;
+        public int count;
+
+        public Slot(Sprite icon, int count)
+        {
+            this.icon = icon;
+            this.count = count;
+        }
+    }
+
     public void AddItem(GameObject item)
     {
         items.Add(item);
--- farm_Project/Assets/UI/Slots.cs
+++ farm_Project/Assets/UI/Slots.cs
@@ -9,7 +9,7 @@ public class Slots : MonoBehaviour
     public Image itemIcon;
     public TextMeshProUGUI quantityText;
 
-    public void SetItem(Inventory.slot slot)
+    public void SetItem(Inventory.Slot slot)
     {
         if (slot != null)
         {
